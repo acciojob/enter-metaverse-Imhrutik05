b48347499e9e15b54ec52819d540a3db24c772a4
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-14 21:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const textE = document.getElementById("status");
+const buttonE = document.getElementById("enterBtn");
+
+
+button.addEventListener("click" , function() {
+	
+	const h1E = document.createElement("h1");
+
+	h1E.textContent = "Entered Metaverse";
+
+	document.body.replaceChild(h1E, textE);
+});
